6eae1fa16b71bc2ec4dc102ea4dc95c77ff9ac5a
Michael Roberts <m_roberts6@u.pacific.edu>
2015-04-04 15:24:07 -0700

    (feat) add reactify and gulp npm dependencies

--- client/src/js/App.jsx
+++ client/src/js/App.jsx
@@ -3,14 +3,13 @@
 var React = require('react');
 
 var Main = React.createClass({
-  render: function() {
+  render: function(){
     return (
-      <div>
-        Turn down for what?
-        <button>
+      <div className="full">
+        <div>ASDF</div>
       </div>
     );
-  }
+  };
 });
 
 React.render(<Main />, document.getElementById('app'));
