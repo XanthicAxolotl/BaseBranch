fc780f92c3a358fe2b5a99cf6791a8afcda1669c
Michael Roberts <m_roberts6@u.pacific.edu>
2015-04-04 17:22:14 -0700

    (feat) add comments around html script tags to enable gulp jsx compiling to work

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,86 @@
 var gulp = require('gulp');
-var reactify = require('reactify');
+var uglify = require('gulp-uglify');
 var htmlreplace = require('gulp-html-replace');
+var source = require('vinyl-source-stream');
 var browserify = require('browserify');
-var react = require('gulp-react');
+// var watchify = require('watchify');
+var reactify = require('reactify');
+var streamify = require('gulp-streamify');
 
 var path = {
   HTML: './client/src/*',
+  CSS: './client/src/css/*.css',
+  MINIFIED_OUT: 'bundle.min.js',
+  OUT: 'bundle.js',
   DEST: './client/dist',
   DEST_BUILD: './client/dist/build',
   DEST_SRC: './client/dist/src',
   ENTRY_POINT: './client/src/js/App.jsx'
 };
 
-// gulp.task('build', function() {
-//   browserify({
-//     entries: [path.ENTRY_POINT],
-//     transform: [reactify]
-//   })
-//     .bundle()
-//     .pipe(gulp.dest(path.DEST_BUILD));
-// });
+gulp.task('build', function(){
+  browserify({
+    entries: [path.ENTRY_POINT],
+    transform: [reactify],
+  })
+    .bundle()
+    .pipe(source(path.MINIFIED_OUT))
+    .pipe(streamify(uglify(path.MINIFIED_OUT)))
+    .pipe(gulp.dest(path.DEST_BUILD));
+});
 
 gulp.task('copy', function(){
   gulp.src(path.HTML)
     .pipe(gulp.dest(path.DEST));
 });
 
-gulp.task('build', function () {
-    return gulp.src(path.ENTRY_POINT)
-        .pipe(react())
-        .pipe(gulp.dest(path.DEST_BUILD));
-});
+// gulp.task('build', function () {
+//   return gulp.src(path.ENTRY_POINT)
+//       .pipe(react())
+//       .pipe(gulp.dest(path.DEST_JS));
+// });
 
 gulp.task('replaceHTML', function(){
   gulp.src(path.HTML)
     .pipe(htmlreplace({
-      //'css': ['./css/main.css', './css/styles.css'],
-      'js': './build/App.js'
+      // 'css': ['./css/main.css', './css/styles.css'],
+      'js': 'build/' + path.MINIFIED_OUT
     }))
     .pipe(gulp.dest(path.DEST));
 });
+
+gulp.task('watch', function() {
+  gulp.watch(path.HTML, ['copy']);
+
+  var watcher  = watchify(browserify({
+    entries: [path.ENTRY_POINT],
+    transform: [reactify],
+    debug: true,
+    cache: {}, packageCache: {}, fullPaths: true
+  }));
+
+  return watcher.on('update', function () {
+    watcher.bundle()
+      .pipe(source(path.OUT))
+      .pipe(gulp.dest(path.DEST_SRC))
+      console.log('Updated');
+  })
+    .bundle()
+    .pipe(source(path.OUT))
+    .pipe(gulp.dest(path.DEST_SRC));
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
