446988ab3830f4f954f38865295b0b332ca5cfd3
Julia Nething <cjnething@gmail.com>
2015-04-06 15:00:43 -0700

    (feat) Create gulp commands and make basic NavBar using material-ui

--- client/src/js/App.jsx
+++ client/src/js/App.jsx
@@ -1,15 +1,25 @@
 // app.jsx
-
 var React = require('react');
+window.React = React;
+var injectTapEventPlugin = require("react-tap-event-plugin");
+var mui = require('material-ui');
+
+//Components
+var NavBar = require('./components/NavBar.jsx');
+
+
+injectTapEventPlugin();
 
 var Main = React.createClass({
   render: function(){
     return (
       <div className="full">
-        <div>ASDF</div>
+        <NavBar />
       </div>
     );
   }
 });
 
 React.render(<Main />, document.getElementById('app'));
+
+
