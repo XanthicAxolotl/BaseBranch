9885e107954bbc21c59bff21bddb1d2a023d35c8
zhengcano <zhengcano@gmail.com>
2015-04-08 15:42:25 -0700

    (feat) add vanilla AJAX requests to CurriculumStore.jsx

--- client/src/js/stores/CurriculumStore.jsx
+++ client/src/js/stores/CurriculumStore.jsx
@@ -1,14 +1,11 @@
+/*==================== REQUIRE MODULES ====================*/
 var Reflux = require('reflux');
 var mui = require('material-ui');
 var Toggle = mui.Toggle;
 
-
+/*================ CREATE CURRICULA STORE =================*/
 var CurriculumStore = Reflux.createStore({
   var _curricula = [];
-  // color: "brown",
-  // width: 900,
-  // height: 560,
-  // channelName: 'placeholderName',
   init: function(){
     this.load();
     // listen to curriculum actions here
@@ -18,42 +15,43 @@ var CurriculumStore = Reflux.createStore({
   load: function(){
     // use this to get the curricula data from the database
     var context = this;
-    var xmlhttp = new XMLHttpRequest();
+    var http = new XMLHttpRequest();
     var url = "https://branchbase.herokuapp.com/api/curriculumview";
 
-    xmlhttp.open("GET", url, true);
-
-    xmlhttp.onreadystatechange = function() {
-      if (xmlhttp.readyState === 4) {
-        _curricula = xmlhttp.response;
+    http.open("GET", url, true);
+    http.setRequestHeader('X-code-lang', 'javascript'/*placeholder language for now*/);
+    http.onreadystatechange = function() {
+      if (http.readyState === 4) {
+        console.log(http.response);
+        _curricula = http.response;
       }
     };
-
-    xmlhttp.send(null);
-
+    http.send(null);
   },
-  pushChanges: function() {
-    // use this to push updates to database
-    // var context = this;
-    // $.ajax({
-    //   type: "POST",
-    //   data: _jobs,
-    //   url: '/api/listings',
-    // }).done(function(data){
-    //     console.log(data);
-    //     context.trigger(_jobs);
-    // });
+  pushChanges: function(curriculum) {
+    var http = new XMLHttpRequest();
+    var url = "https://branchbase.herokuapp.com/api/curriculumview";
+    var context = this;
+
+    http.open("PUSH", url, true);
+    http.setRequestHeader('Content-type', 'application/json');
+    http.onreadystatechange = function() {
+      if (http.readyState === 4) {
+        context.trigger(_curricula);
+      }
+    };
+    http.send(JSON.stringify(curriculum));
   },
-  onEdit: function(job) {
+  onEdit: function(curriculum) {
     // console.log("from onEdit in jobStore.jsx");
-    // for (var i = 0; i < _jobs.length; i++) {
-    //   if(_jobs[i]._id === job._id) {
-    //     _jobs[i].mutable = job.mutable;
-    //     this.trigger(_jobs);
-    //     break;
-    //   }
-    // }
-    // this.pushChanges();
+    for (var i = 0; i < _curricula.length; i++) {
+      if(_curricula[i]._id === curriculum._id) {
+        _curricula[i].rating = curriculum.rating
+        this.trigger(_curricula);
+        break;
+      }
+    }
+    this.pushChanges(curriculum);
   }
 });
 
