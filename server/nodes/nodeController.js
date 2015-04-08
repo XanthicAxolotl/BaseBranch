30e550918169424ae7ea2940a8585fd79a1766fc
Wendy Leung <wendysarahleung@gmail.com>
2015-04-07 19:15:48 -0700

    (feat) Implement createNode method

--- server/nodes/nodeController.js
+++ server/nodes/nodeController.js
@@ -1,10 +1,25 @@
+var Nodes = require('../config/db_models.js').Nodes;
+
+
 module.exports = {
 
   createNode: function(req, res, next) {
     // TODO: Implement
+
+    Nodes.create({
+      name: req.body.name
+    })
+    .then(function(resource) {
+      console.log('Successfully created node in database');
+      res.json(resource);
+    })
+    .error(function(err){
+      console.error('Error in creating instance of node: ', err);
+    });
   },
 
   getResources: function(req, res, next) {
     // TODO: Implement
+    
   }
 };
