f0c8b92275b583b1fff7a2e5a0006ba63c829961
zhengcano <zhengcano@gmail.com>
2015-04-07 19:53:39 -0700

    (feat) add inline styles to curriculum view

--- client/src/js/components/CurriculumView.jsx
+++ client/src/js/components/CurriculumView.jsx
@@ -17,9 +17,48 @@ var injectTapEventPlugin = require("react-tap-event-plugin");
 
 injectTapEventPlugin();
 
+var curriculumContainerStyle = {
+                                 display: '-webkit-box',
+                                 display: '-webkit-flex',
+                                 display: '-ms-flexbox',
+                                 display: 'flex',
+                                 WebkitBoxOrient: 'vertical',
+                                 WebkitBoxDirection: 'reverse',
+                                 WebkitFlexDirection: 'column',
+                                 msFlexDirection: 'column',
+                                 flexDirection: 'column',
+                               };
+var singleCurriculumStyle = {
+                              display: '-webkit-box',
+                              display: '-webkit-flex',
+                              display: '-ms-flexbox',
+                              display: 'flex',
+                              WebkitBoxOrient: 'horizontal',
+                              WebkitBoxDirection: 'normal',
+                              WebkitFlexDirection: 'row',
+                              msFlexDirection: 'row',
+                              flexDirection: 'row'
+                            }
+var resourceTitleStyle = {
+                            display: '-webkit-box',
+                            display: '-webkit-flex',
+                            display: '-ms-flexbox',
+                            display: 'flex',
+                            WebkitBoxOrient: 'horizontal',
+                            WebkitBoxDirection: 'normal',
+                            WebkitFlexDirection: 'row',
+                            msFlexDirection: 'row',
+                            flexDirection: 'row',
+                            WebkitBoxPack: 'justify',
+                            WebkitJustifyContent: 'space-between',
+                            msFlexPack: 'justify',
+                            justifyContent: 'space-between'
+                          }
+
 // these should be populated by the database
 var menuItems = [{
                   id: '1',
+                  name: 'Super Xanthic Curriculum!',
                   desc:'Example Framework 1',
                   author:'Xanthic Axolotl',
                   anchor:'http://www.walmart.com',
@@ -29,12 +68,17 @@ var menuItems = [{
                 },
                 {
                   id: '2',
+                  name: 'Super Xanthic Curriculum!',
                   desc:'Example Framework 1',
                   author:'Xanthic Axolotl',
                   anchor:'http://www.walmart.com',
                   update: new Date().getDate,
                   rating: 20,
-                  resources: [{key: 1, name: 'Super Awesome Javascript Blog!'}]
+                  resources: [{key: 1, name: 'Super Awesome Javascript Blog!'},
+                              {key: 2, name: 'Super Awesome Javascript Blog!'},
+                              {key: 3, name: 'Super Awesome Javascript Blog!'},
+                              {key: 4, name: 'Super Awesome Javascript Blog!'},
+                              {key: 5, name: 'Super Awesome Javascript Blog!'}]
                 }];
 
 var ItemView = React.createClass({
@@ -43,16 +87,18 @@ var ItemView = React.createClass({
       return <li>{resource.name}</li>
     });
     return(
-      <li>
-        <ul>
-          <li><span>{this.props.name}</span><a href={this.props.anchor}>View Curriculum</a></li>
-          <li>{this.props.desc}</li>
-          <li>Created By: {this.props.author}</li>
-          <li>Last Updated: {this.props.update}</li>
-          <li>Rating: {this.props.rating}</li>
-        </ul>
+      <li class="single-curriculum" style={singleCurriculumStyle}>
         <div>
-          <ul>
+          <ul class="curriculum-info">
+            <li class="resource-title" style={resourceTitleStyle}><div>{this.props.name}</div><a href={this.props.anchor}>View Curriculum</a></li>
+            <li>{this.props.desc}</li>
+            <li>Created By: {this.props.author}</li>
+            <li>Last Updated: {this.props.update}</li>
+            <li>Rating: {this.props.rating}</li>
+          </ul>
+        </div>
+        <div>
+          <ul class="resource-list">
             {resources}
           </ul>
         </div>
@@ -62,7 +108,6 @@ var ItemView = React.createClass({
 });
 
 var CurriculumView = React.createClass({
-
   render: function() {
     var curry = menuItems.map(function(result) {
       return <ItemView key={result.id} name={result.name} desc={result.desc} author={result.author} anchor={result.src} update={result.update} rating={result.rating} resources={result.resources}/>
@@ -70,7 +115,7 @@ var CurriculumView = React.createClass({
 
     return (
       <div>
-        <ul>
+        <ul class="curriculum-container" style={curriculumContainerStyle}>
           {curry}
         </ul>
       </div>
