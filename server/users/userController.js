9be61831b149a188ffe6237323975fdc32400e15
Wendy Leung <wendysarahleung@gmail.com>
2015-04-08 17:14:09 -0700

    (feat) Implement createUser and getUser methods on userController.

--- server/users/userController.js
+++ server/users/userController.js
@@ -1,11 +1,37 @@
+var Users = require('../config/db_models.js').Users;
+
 module.exports = {
 
   createUser: function(req, res, next) {
     // TODO: Implement
+    var body = req.body;
+
+    Users.create({
+      name: body.name,
+      password: body.password,
+      email: body.email,
+      reputation: body.reputation,
+      picture: body.picture
+    })
+    .then(function(user) {
+      console.log('Successfully created user in database');
+      res.json(user);
+    })
+    .error(function(err){
+      console.error('Error in creating instance of user: ', err);
+    });
   },
 
   getUser: function(req, res, next) {
     // TODO: Implement
+    Users.find({ where:{ id: req.params.userId } })
+    .then(function(user){
+      console.log('Successfully found user', user);
+      res.json(user);
+    })
+    .error(function(err){
+      console.error('Error in finding user', err);
+    })
   },
 
   updateReputation: function(req, res, next) {
