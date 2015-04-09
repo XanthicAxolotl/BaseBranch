d444312b43a773c7f426027258d59231ed1afa4d
Wendy Leung <wendysarahleung@gmail.com>
2015-04-08 19:47:24 -0700

    (feat) Add all methods in userController.

--- server/users/userController.js
+++ server/users/userController.js
@@ -1,4 +1,5 @@
 var Users = require('../config/db_models.js').Users;
+var Curricula = require('../config/db_models.js').Curricula;
 
 module.exports = {
 
@@ -36,21 +37,85 @@ module.exports = {
 
   updateReputation: function(req, res, next) {
     // TODO: Implement
+    Users.update(
+    {
+      reputation: req.body.reputation
+    },
+    {
+      where: { id: req.params.userId}
+    })
+    .success(function(){
+      console.log('Successfully updated reputation')
+    })
+    .error(function(){
+      console.log('Error to updating reputation')
+    });
   },
 
   uploadPicture: function(req, res, next) {
     // TODO: Implement
+    Users.update(
+    {
+      picture: req.body.picture
+    },
+    {
+      where: { id: req.params.userId}
+    })
+    .success(function(){
+      console.log('Successfully updated picture')
+    })
+    .error(function(){
+      console.log('Error to updating picture')
+    });
   },
 
   getCreatedCurricula: function(req, res, next) {
     // TODO: Implement
+    Curricula.findAll({where:{ userId: req.params.userId }})
+    .then(function(curricula){
+      console.log('Successfully found all curricula', curricula);
+      res.json(curricula);
+    })
+    .error(function(err){
+      console.error('Error in finding all curricula:', err);
+    })
   },
 
   getSubscribedCurricula: function(req, res, next) {
     // TODO: Implement
+    Users.find({ where:{ id: req.params.userId } })
+    .then(function(user){
+      console.log('Successfully found user to get subscriptions', user);
+      user.getCurriculas({where: {userId: req.body.userId }})
+      .then(function(curricula){
+        console.log('Successfully retrieve curricula user is subscribed to', curricula)
+        res.json(curricula);
+      })
+      .error(function(err){
+        console.log('Error in retrieving curricula user is subscribed to', err)
+      })
+    })
+    .error(function(err){
+      console.error('Error in finding user to get subscriptions', err);
+    })
   },
 
   subscribeToCurriculum: function(req, res, next) {
     // TODO: Implement
+    Users.find({ where:{ id: req.params.userId } })
+    .then(function(user){
+      console.log('Successfully found user for subscription', user);
+      user.addCurricula(req.body.curriculaId)
+      .then(function(curricula){
+        console.log('Successfully add subscription for user');
+        res.json(curricula);
+      })
+      .error(function(err){
+        console.log('Error in adding subscription for user', err);
+      })
+    })
+    .error(function(err){
+      console.error('Error in finding user for subscription', err);
+    })
   }
 };
