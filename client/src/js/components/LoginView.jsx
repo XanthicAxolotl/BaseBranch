0792b1886ad0c720ad1c7cc6586f1607341f5bb9
zhengcano <zhengcano@gmail.com>
2015-04-13 11:20:01 -0700

    (feat) implement login view to html file

--- client/src/js/components/LoginView.jsx
+++ client/src/js/components/LoginView.jsx
@@ -2,19 +2,22 @@ var React = require('react');
 var mui = require('material-ui');
 var TextField = mui.TextField;
 var RaisedButton = mui.RaisedButton;
+var LoginStyles = require('../styles/LoginStyles.js');
 
 var LoginView = React.createClass({
-  render: function(){ 
+  render: function() { 
     return (
-      <div className="full">
-        <h1>Log In</h1>
-        <TextField
-          hintText="Username" /> <br />
-        <TextField
-          hintText="Password"/> <br />
-        <RaisedButton label="Log In" secondary={true} />
-        <RaisedButton label="Log In with GitHub" secondary={true} />
-        <p><a href="/">Don't have an account? Sign up here.</a></p>
+      <div className="login-main" styles={LoginStyles.main}>
+        <div>
+          <h1>Log In</h1>
+          <TextField
+            hintText="Username" /> <br />
+          <TextField
+            hintText="Password"/> <br />
+          <RaisedButton label="Log In" secondary={true} />
+          <RaisedButton label="Log In with GitHub" secondary={true} />
+          <p><a href="./">Dont have an account? Sign up here.</a></p>
+        </div>
       </div>
     );
   }
