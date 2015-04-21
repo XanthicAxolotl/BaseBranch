0b51b656faa35e7966be958b7493038cdd656511
zhengcano <zhengcano@gmail.com>
2015-04-20 19:41:42 -0700

    (feat) add profile button to nav bar after login

--- client/src/js/components/PopularView.jsx
+++ client/src/js/components/PopularView.jsx
@@ -10,22 +10,22 @@ var PopularView = React.createClass({
           <h3>Popular Languages</h3>
           <div className="inner-container">
             <a href='#'>
-              <Paper className="box" zDepth={1} rounded={false}>
+              <Paper className="box" zDepth={1} rounded={true}>
                 <img className="popular-image" src="./images/javascript-icon.png" /><h5>Javascript</h5>
               </Paper>
             </a>
             <a href='#'>
-              <Paper className="box" zDepth={1} rounded={false}>
+              <Paper className="box" zDepth={1} rounded={true}>
                 <img className="popular-image" src="./images/react-icon.png" /><h5>React</h5>
               </Paper>
             </a>
             <a href='#'>
-              <Paper className="box" zDepth={1} rounded={false}>
+              <Paper className="box" zDepth={1} rounded={true}>
                 <img className="popular-image" src="./images/angular.png" /><h5>Angular</h5>
               </Paper>
             </a>
             <a href='#'>
-              <Paper className="box" zDepth={1} rounded={false}>
+              <Paper className="box" zDepth={1} rounded={true}>
                 <img className="popular-image" src="./images/backbone.png" /><h5>Backbone</h5>
               </Paper>
             </a>
@@ -35,22 +35,22 @@ var PopularView = React.createClass({
           <h3>Popular Curricula</h3>
           <div className="inner-container">  
             <a href='#'>
-              <Paper className="box" zDepth={1} rounded={false}>
+              <Paper className="box" zDepth={1} rounded={true}>
                 <img className="popular-image" src="./images/javascript-icon.png" /><h5>Rubyist Guilde</h5>
               </Paper>
             </a>
             <a href='#'>
-              <Paper className="box" zDepth={1} rounded={false}>
+              <Paper className="box" zDepth={1} rounded={true}>
                 <img className="popular-image" src="./images/javascript-icon.png" /><h5>Javascript Ninja</h5>
               </Paper>
             </a>
             <a href='#'>
-              <Paper className="box" zDepth={1} rounded={false}>
+              <Paper className="box" zDepth={1} rounded={true}>
                 <img className="popular-image" src="./images/javascript-icon.png" /><h5>Pftt PHP</h5>
               </Paper>
             </a>
             <a href='#'>
-              <Paper className="box" zDepth={1} rounded={false}>
+              <Paper className="box" zDepth={1} rounded={true}>
                 <img className="popular-image" src="./images/javascript-icon.png" /><h5>Python Marathon</h5>
               </Paper>
             </a>
